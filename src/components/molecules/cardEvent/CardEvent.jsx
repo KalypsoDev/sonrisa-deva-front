cc979cf3747ac4e201cf7b5b3b4b13313d6a3921
Claudia González <claudia.glezg@gmail.com>
2024-04-17 13:02:10 +0200

    Add the date icon and modify the size and color

--- src/components/molecules/cardEvent/CardEvent.jsx
+++ src/components/molecules/cardEvent/CardEvent.jsx
@@ -13,20 +13,34 @@ const CardEvent = () => {
       />
       <div className="flex flex-col justify-between p-4 leading-normal w-full">
         <div className="text-center mb-4">
-          <h5 className="text-2xl font-bold text-blue-950 dark:text-white">
+          <h5 className="text-2xl font-bold text-darkBlue dark:text-white">
             Título del Evento
           </h5>
         </div>
         <div className="mb-4">
-          <p className="font-bold text-blue-950 dark:text-blue-950">Fecha:</p>
-          <p className="font-bold text-blue-950 dark:text-blue-950">Hora:</p>
-          <p className="font-bold text-blue-950 dark:text-blue-950">Lugar:</p>
+        <div className="flex items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+        className="w-6 h-6 mr-2 text-darkBlue"
+      >
+        <path
+          fill="currentColor"
+          d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"
+        />
+      </svg>
+      <p className="font-bold text-darkBlue dark:text-blue-950">
+        Fecha:
+      </p>
+    </div>
+          <p className="font-bold text-darkBlue dark:text-blue-950">Hora:</p>
+          <p className="font-bold text-darkBlue dark:text-blue-950">Lugar:</p>
         </div>
         <div className="text-center">
-          <p className="font-bold text-blue-950 dark:text-blue-950">
+          <p className="font-bold text-darkBlue dark:text-blue-950">
             Recaudación
           </p>
-          <p className="font-bold text-blue-950 dark:text-blue-950 text-3xl">
+          <p className="font-bold text-darkBlue dark:text-blue-950 text-3xl">
             2000€
           </p>
         </div>

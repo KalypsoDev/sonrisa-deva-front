f1f9a8a95d1389c97cfee469a9eb1b14a4891d67
Claudia González <claudia.glezg@gmail.com>
2024-04-17 20:51:25 +0200

    Add ProductForm Route

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { HomePage } from '../components/pages/homepage/HomePage';
+import ProductFormPage from '../components/pages/productFormPage/ProductFormPage';
 
 const Router = () => {
     return (
       <BrowserRouter>
           <Routes>
                   <Route path='/' element={<HomePage/>}></Route>
+                  <Route path='/product-form' element={<ProductFormPage/>}></Route>
           </Routes>
       </BrowserRouter>
     )

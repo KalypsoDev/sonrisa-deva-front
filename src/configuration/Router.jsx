ecc8f2302a283f0029202afff094a038e10a4a83
Desiree <desireesanchezdev@gmail.com>
2024-04-17 12:19:38 +0200

    Create Route for ProductPage

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { HomePage } from '../components/pages/homepage/HomePage';
+import ProductPage from '../components/pages/productPage/ProductPage';
 
 const Router = () => {
     return (
       <BrowserRouter>
           <Routes>
                   <Route path='/' element={<HomePage/>}></Route>
+                  <Route path='/tienda-solidaria' element={<ProductPage/>}></Route>
           </Routes>
       </BrowserRouter>
     )

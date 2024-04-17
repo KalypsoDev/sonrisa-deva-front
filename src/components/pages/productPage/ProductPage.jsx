05f0738dd3529ff625d903b4ce1083b60e606c36
Desiree <desireesanchezdev@gmail.com>
2024-04-17 12:18:09 +0200

    Create ProductPage

---
+++ src/components/pages/productPage/ProductPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CardProduct from '../../molecules/cardProduct/CardProduct'
+
+
+const ProductPage = () => {
+  return (
+    <CardProduct/>
+  )
+}
+
+export default ProductPage

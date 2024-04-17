b18b3eef4c413f717a7a530735f4589018431f8a
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-17 13:00:11 +0200

    Add sidebar code from flowbite and customize it

---
+++ src/components/molecules/sidebar/Sidebar.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import sonrisaDevaSmallLogo from "../../../assets/img/smallLogo.jpg";
+
+const Sidebar = () => {
+    return (
+        <>
+            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                <span class="sr-only">Open sidebar</span>
+                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                </svg>
+            </button>
+
+            <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-46 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+
+                <div class="h-full px-4 py-3 overflow-y-auto bg-primaryLila dark:bg-gray-800">
+                    <a href="#" class="flex items-center p-2 text-white">
+                        <span class="flex flex-col items-center">
+                            <span class="whitespace-nowrap font-bold">Panel de</span>
+                            <span class="whitespace-nowrap ml-3 font-bold">Administración</span>
+                        </span>
+                    </a>
+
+                    <a href="#" class="flex items-center justify-center ps-2.5 mb-5">
+                        <img class="rounded-full w-20 h-20 border-2 border-primaryBlue" src={sonrisaDevaSmallLogo} alt="Sonrisa de Deva Logo" />
+                    </a>
+
+                    <ul class="space-y-2 font-normal">
+                        <li>
+                            <a href="#" class="flex items-center p-1 text-white">
+                                <span class="ms-3">Gestión de la página</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="flex items-center p-1 text-white">
+
+                                <span class="flex-1 ms-3 whitespace-nowrap">Productos solicitados</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-1 text-white">
+                                <span class="flex-1 ms-3 whitespace-nowrap">Agenda de eventos</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-1 text-white">
+                                <span class="flex-1 ms-3 whitespace-nowrap">Agregar productos</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-1 text-white">
+                                <span class="flex-1 ms-3 whitespace-nowrap">Gestión de productos</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-1 text-white">
+
+                                <span class="flex-1 ms-3 whitespace-nowrap">Ir a mi página</span>
+                            </a>
+                        </li>
+
+                    </ul>
+
+                    <button type="button" class="text-white bg-darkBlue hover:bg-primaryBlue focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mx-auto">
+                        Cerrar sesión
+                    </button>
+                </div>
+            </aside>
+        </>
+    )
+}
+
+export default Sidebar

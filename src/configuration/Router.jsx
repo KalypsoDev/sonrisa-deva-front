470c2b8216570e1ed94ed6cea05ece4881ff6c43
Claudia González <claudia.glezg@gmail.com>
2024-04-17 11:57:20 +0200

    Change Route for EventPage

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { HomePage } from '../components/pages/homepage/HomePage';
+import EventPage from '../components/pages/eventpage/EventPage';
 
 const Router = () => {
     return (
       <BrowserRouter>
           <Routes>
                   <Route path='/' element={<HomePage/>}></Route>
+                  <Route path='/eventos' element={<EventPage/>}></Route>
           </Routes>
       </BrowserRouter>
     )

4f1cec5c5e5901abbca9e90fb37f314f18860b89
Desiree <desireesanchezdev@gmail.com>
2024-04-17 14:48:58 +0200

    Delete path svg on CardProduct Molecule

--- src/components/molecules/cardProduct/CardProduct.jsx
+++ src/components/molecules/cardProduct/CardProduct.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 const CardProduct = () => {
     return (
         <div className="max-w-64 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <img className="rounded-t-lg w-48" src="src\assets\img\productBoli.jpg" alt="" />
-            <div className="p-5">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </a>
+            <img className="rounded-t-lg w-full h-40" src="src\assets\img\productBoli.jpg" alt="" />
+            <div className="p-3">
+                <h4 className="mb-2 text-lg font-bold tracking-tight text-darkBlue dark:text-white">Bolígrafos</h4>
+                <div className='p-1'> 
+                    <p className=" text-xs font-normal text-gray-700 dark:text-gray-400">Tallas s, m, l, xl</p>
+                </div>
+                <h5 className="mb-3 text-lg font-bold text-darkBlue dark:text-gray-400">DONATIVO: 5€</h5>
+                <div className='flex justify-center'>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-darkBlue rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Lo quiero
+                    </a>
+                </div>
             </div>
         </div>
     )

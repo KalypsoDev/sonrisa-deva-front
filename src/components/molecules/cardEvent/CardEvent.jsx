aa510f901da80b03d4bacffe72d06972b8443289
Claudia González <claudia.glezg@gmail.com>
2024-04-17 12:11:27 +0200

    Modify disposition in CardEvent molecule

--- src/components/molecules/cardEvent/CardEvent.jsx
+++ src/components/molecules/cardEvent/CardEvent.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const CardEvent = () => {
   return (
     <a
       href="#"
-      className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
     >
       <img
-        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-        src="/docs/images/blog/image-4.jpg"
+        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/mafalda.jpg"
         alt=""
       />
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
-        </h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-        </p>
+      <div className="flex flex-col justify-between p-4 leading-normal w-full">
+        <div className="text-center mb-4">
+          <h5 className="text-2xl font-bold text-blue-950 dark:text-white">
+            Título del Evento
+          </h5>
+        </div>
+        <div className="mb-4">
+          <p className="font-bold text-blue-950 dark:text-blue-950">Fecha:</p>
+          <p className="font-bold text-blue-950 dark:text-blue-950">Hora:</p>
+          <p className="font-bold text-blue-950 dark:text-blue-950">Lugar:</p>
+        </div>
+        <div className="text-center">
+          <p className="font-bold text-blue-950 dark:text-blue-950">
+            Recaudación
+          </p>
+          <p className="font-bold text-blue-950 dark:text-blue-950 text-3xl">
+            2000€
+          </p>
+        </div>
       </div>
     </a>
   );

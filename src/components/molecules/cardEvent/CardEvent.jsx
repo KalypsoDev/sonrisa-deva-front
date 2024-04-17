3f2d3f787505137257d4c2b12af5eb9e8b21c318
Claudia González <claudia.glezg@gmail.com>
2024-04-17 14:28:18 +0200

    Add hover to the image

--- src/components/molecules/cardEvent/CardEvent.jsx
+++ src/components/molecules/cardEvent/CardEvent.jsx
@@ -7,9 +7,10 @@ const CardEvent = () => {
       className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
     >
       <img
-        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg hover:scale-150"
         src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/mafalda.jpg"
         alt=""
+        
       />
       <div className="flex flex-col justify-between p-4 leading-normal w-full">
         <div className="text-center mb-4">
@@ -47,18 +48,23 @@ const CardEvent = () => {
           <div className="flex items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
+              viewBox="0 0 384 512"
               className="w-6 h-6 mr-2 text-darkBlue"
             >
               <path
                 fill="currentColor"
-                d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"
-              />
+                d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
             </svg>
-            <p className="font-bold text-darkBlue dark:text-blue-950">Fecha:</p>
+            <p className="font-bold text-darkBlue dark:text-blue-950">Lugar:</p>
           </div>
         </div>
-        <div className="text-center">
+        <div className="flex items-center justify-center mb-2">
+        <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 576 512" 
+        className="w-8 h-8 mr-2 text-darkBlue">
+        <path fill="currentColor" d="M163.9 136.9c-29.4-29.8-29.4-78.2 0-108s77-29.8 106.4 0l17.7 18 17.7-18c29.4-29.8 77-29.8 106.4 0s29.4 78.2 0 108L310.5 240.1c-6.2 6.3-14.3 9.4-22.5 9.4s-16.3-3.1-22.5-9.4L163.9 136.9zM568.2 336.3c13.1 17.8 9.3 42.8-8.5 55.9L433.1 485.5c-23.4 17.2-51.6 26.5-80.7 26.5H192 32c-17.7 0-32-14.3-32-32V416c0-17.7 14.3-32 32-32H68.8l44.9-36c22.7-18.2 50.9-28 80-28H272h16 64c17.7 0 32 14.3 32 32s-14.3 32-32 32H288 272c-8.8 0-16 7.2-16 16s7.2 16 16 16H392.6l119.7-88.2c17.8-13.1 42.8-9.3 55.9 8.5zM193.6 384l0 0-.9 0c.3 0 .6 0 .9 0z"/>
+        </svg>
           <p className="font-bold text-darkBlue dark:text-blue-950">
             Recaudación
           </p>

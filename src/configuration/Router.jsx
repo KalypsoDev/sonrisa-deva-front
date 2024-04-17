d3ce2bfe50f9b3d53782c1bfbfb36fdbf5b53247
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-17 20:11:53 +0200

    Add route for ContactFormPage

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { HomePage } from '../components/pages/homepage/HomePage';
+import ContactFormPage from '../components/pages/contactForm/ContactFormPage';
 
 const Router = () => {
     return (
       <BrowserRouter>
           <Routes>
                   <Route path='/' element={<HomePage/>}></Route>
+                  <Route path='/contact-form' element={<ContactFormPage/>}></Route>
           </Routes>
       </BrowserRouter>
     )

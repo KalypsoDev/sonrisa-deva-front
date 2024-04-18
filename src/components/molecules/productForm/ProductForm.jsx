f27e12e03b0f1c49057709bdd824c245acf81acd
Claudia González <claudia.glezg@gmail.com>
2024-04-18 18:19:23 +0200

    Change the position of the inputs

--- src/components/molecules/productForm/ProductForm.jsx
+++ src/components/molecules/productForm/ProductForm.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ProductForm = () => {
   return (
-    <div className="bg-darkBlue dark:bg-gray-800 rounded-2xl shadow-lg p-4 max-w-screen-lg mx-auto my-8">
-    <section className="bg-darkBlue dark:bg-gray-900">
+    <div className="bg-darkBlue dark:bg-gray-800 rounded-lg shadow-lg p-6 max-w-4xl mx-auto my-8">
       <div className="p-4">
         <form action="#">
           <div className="grid gap-4 sm:grid-cols-2">
+            <div>
             <div className="w-full">
               <label htmlFor="name" className="block mb-2 text-sm font-medium text-white dark:text-white">Nombre</label>
               <input
@@ -19,18 +19,7 @@ const ProductForm = () => {
               />
             </div>
             <div className="w-full">
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-white dark:text-white">Email</label>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Nombre"
-                required
-              />
-            </div>
-            <div className="w-full">
-              <label htmlFor="apellido" className="block mb-2 text-sm font-medium text-white dark:text-white">Apellido</label>
+              <label htmlFor="apellido" className="block mb-2 mt-2 text-sm font-medium text-white dark:text-white">Apellido</label>
               <input
                 type="text"
                 name="apellido"
@@ -41,18 +30,7 @@ const ProductForm = () => {
               />
             </div>
             <div className="w-full">
-              <label htmlFor="telefono" className="block mb-2 text-sm font-medium text-white dark:text-white">Teléfono</label>
-              <input
-                type="number"
-                name="telefono"
-                id="telefono"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Teléfono"
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="direccion" className="block mb-2 text-sm font-medium text-white dark:text-white">Dirección</label>
+              <label htmlFor="direccion" className="block mb-2 mt-2 text-sm font-medium text-white dark:text-white">Dirección</label>
               <input
                 type="text"
                 name="direccion"
@@ -63,13 +41,38 @@ const ProductForm = () => {
               />
             </div>
             <div className="w-full">
-              <label htmlFor="mensaje" className="block mb-2 text-sm font-medium text-white dark:text-white">Mensaje (Requerido)</label>
+              <label htmlFor="telefono" className="block mb-2 mt-2 text-sm font-medium text-white dark:text-white">Teléfono</label>
+              <input
+                type="number"
+                name="telefono"
+                id="telefono"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Teléfono"
+                required
+              />
+            </div>
+            </div>
+            <div>
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-white dark:text-white">Email</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="w-full">
+              <label htmlFor="mensaje" className="block mb-2 mt-8 text-sm font-medium text-white dark:text-white">Mensaje (Requerido)</label>
               <textarea
                 id="mensaje"
-                rows="4"
+                rows="6"
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Especifica talla o color deseado"
+                placeholder="Especifica talla o color del producto seleccionado según sea el caso"
               ></textarea>
+            </div>
             </div>
           </div>
           <div className="text-center mt-6">
@@ -88,7 +91,6 @@ const ProductForm = () => {
           </div>
         </form>
       </div>
-    </section>
     </div>
   );
 };

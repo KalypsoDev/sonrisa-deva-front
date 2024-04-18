8868404a7de3702ffb573861ffc4d55b4cc7c9f6
Desiree <desireesanchezdev@gmail.com>
2024-04-18 08:48:12 +0200

    Add NewProductAdminPage on Routes

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { HomePage } from '../components/pages/homepage/HomePage';
+import NewProductAdminPage from '../components/pages/newProductAdminPage/NewProductAdminPage';
 
 const Router = () => {
     return (
       <BrowserRouter>
           <Routes>
                   <Route path='/' element={<HomePage/>}></Route>
+                  <Route path='/create-product' element={<NewProductAdminPage/>}></Route>
           </Routes>
       </BrowserRouter>
     )

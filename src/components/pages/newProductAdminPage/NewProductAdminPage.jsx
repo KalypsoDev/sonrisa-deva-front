699120a2bb48a917d44c7976f69693a0f261a18c
Desiree <desireesanchezdev@gmail.com>
2024-04-18 12:41:27 +0200

    Add Sidebar on NewProductAdminPage

--- src/components/pages/newProductAdminPage/NewProductAdminPage.jsx
+++ src/components/pages/newProductAdminPage/NewProductAdminPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ProductFormAdmin from '../../molecules/productFormAdmin/ProductFormAdmin'
+import Sidebar from '../../molecules/sidebar/Sidebar'
 
 const NewProductAdminPage = () => {
   return (
     <>
+    <Sidebar/>
     <ProductFormAdmin/>
     </>
   )

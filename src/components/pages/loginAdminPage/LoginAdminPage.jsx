2d0d75aec6b2c304e69f31ea7a1928cc2f97d0b7
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-18 20:32:18 +0200

    Add loginFormAdmin folder and file

---
+++ src/components/pages/loginAdminPage/LoginAdminPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../../molecules/loginFormAdmin/LoginFormAdmin'
+
+const LoginAdminPage = () => {
+  return (
+    <>
+    <LoginFormAdmin/>
+    </>
+  )
+}
+
+export default LoginAdminPage

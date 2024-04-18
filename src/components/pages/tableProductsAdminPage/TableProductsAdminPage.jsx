a32301cdf464df75a7b0c88810dd5ecf21f50e4b
Claudia González <claudia.glezg@gmail.com>
2024-04-18 20:32:28 +0200

    Add TableProducts component to the page

---
+++ src/components/pages/tableProductsAdminPage/TableProductsAdminPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TableProducts from '../../molecules/tableProducts/TableProducts'
+
+const TableProductsAdminPage = () => {
+  return (
+   <TableProducts />
+  )
+}
+
+export default TableProductsAdminPage

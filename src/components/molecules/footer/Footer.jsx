c4df6d141bef30118f21aebca10d82ed5554f5ca
KalypsoDev <antunezangela89@gmail.com>
2024-04-18 12:11:55 +0200

    Add and import logo of La Sonrisa de Deva

--- src/components/molecules/footer/Footer.jsx
+++ src/components/molecules/footer/Footer.jsx
@@ -1,12 +1,13 @@
+import sonrisaDevaSmallLogo from "../../../assets/img/smallLogo.jpg";
+
 export function Footer() {
     return (
       <>
         <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <a href="#" className="flex justify-center mb-6">
+                        <img className="rounded-full w-24 h-24 border-2 border-primaryBlue" src={sonrisaDevaSmallLogo} alt="Sonrisa de Deva Logo" />
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>

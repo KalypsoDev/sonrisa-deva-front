c3d8024b639537f3fc3fe99d10702510fd65b16e
KalypsoDev <antunezangela89@gmail.com>
2024-04-18 18:54:52 +0200

    Import Footer as RAFCE

--- src/components/pages/productPage/ProductPage.jsx
+++ src/components/pages/productPage/ProductPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavbarMolecule } from "../../molecules/navbar/NavbarMolecule";
-import { Footer } from "../../molecules/footer/Footer";
+import Footer from "../../molecules/footer/Footer";
 import CardProduct from '../../molecules/cardProduct/CardProduct';
 
 

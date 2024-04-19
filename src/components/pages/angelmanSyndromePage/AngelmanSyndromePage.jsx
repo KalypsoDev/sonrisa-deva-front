446d30f87db1852e0bcaa4b199b85ee8afb71b0d
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-19 12:59:41 +0200

    Create angelmanSyndromePage folder and file

---
+++ src/components/pages/angelmanSyndromePage/AngelmanSyndromePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AngelmanSyndrome from '../../molecules/angelmanSyndrome/AngelmanSyndrome'
+
+const AngelmanSyndromePage = () => {
+  return (
+    <>
+    <AngelmanSyndrome/>
+    </>
+  )
+}
+
+export default AngelmanSyndromePage

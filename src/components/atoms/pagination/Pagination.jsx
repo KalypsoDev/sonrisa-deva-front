651582494fd1bece7b8b6b430b1a0047e67dad66
Desiree <desireesanchezdev@gmail.com>
2024-04-19 13:48:22 +0200

    Delete dark classes on Pagination

--- src/components/atoms/pagination/Pagination.jsx
+++ src/components/atoms/pagination/Pagination.jsx
@@ -14,7 +14,7 @@ const Pagination = ({currentPage, perPage, totalItems, onPageChange }) => {
             <ul className="inline-flex justify-center -space-x-px text-sm">
                 <li>
                     <button
-                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                        className="flex items-center justify-center px-3 h-8 leading-tight text-darkGrey bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
                         onClick={() => handlePageClick(currentPage - 1)}
                         disabled={currentPage === 1}
                     >
@@ -24,7 +24,7 @@ const Pagination = ({currentPage, perPage, totalItems, onPageChange }) => {
                 {Array.from({ length: pageCount }, (_, i) => (
                     <li key={i}>
                         <button
-                            className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${currentPage === i + 1 ? 'text-blue-600 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:text-white' : ''
+                            className={`flex items-center justify-center px-3 h-8 leading-tight text-darkGrey bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${currentPage === i + 1 ? 'text-blue-600 bg-blue-50 hover:bg-blue-100 hover:text-blue-700' : ''
                                 }`}
                             onClick={() => handlePageClick(i + 1)}
                         >
@@ -34,7 +34,7 @@ const Pagination = ({currentPage, perPage, totalItems, onPageChange }) => {
                 ))}
                 <li>
                     <button
-                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
+                        className="flex items-center justify-center px-3 h-8 leading-tight text-darkGrey bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
                         onClick={() => handlePageClick(currentPage + 1)}
                         disabled={currentPage === pageCount}
                     >

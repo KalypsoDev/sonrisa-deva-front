e2274d528d786e3233d0234540b89af253ab2a8a
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-19 19:32:50 +0200

    Import Carousel component in HomePage

--- src/components/pages/homepage/HomePage.jsx
+++ src/components/pages/homepage/HomePage.jsx
@@ -1,11 +1,13 @@
 import  {NavbarMolecule}  from "../../molecules/navbar/NavbarMolecule";
 import  {Footer}  from "../../molecules/footer/Footer";
+import  {Carousel}  from "../../molecules/carousel/Carousel";
 
 
 export function HomePage() {
   return (
     <>
         <NavbarMolecule></NavbarMolecule>
+        <Carousel></Carousel>
         <h1 className="font-montserratBold">Ésta es la home page</h1>
         <Footer></Footer>
     </>

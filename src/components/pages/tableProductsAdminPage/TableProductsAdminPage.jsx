26ce02594b4be4d4802460693f40fa18d4e7c277
Claudia González <claudia.glezg@gmail.com>
2024-04-19 09:38:45 +0200

    Delete sidebar in the page

--- src/components/pages/tableProductsAdminPage/TableProductsAdminPage.jsx
+++ src/components/pages/tableProductsAdminPage/TableProductsAdminPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import TableProducts from '../../molecules/tableProducts/TableProducts'
-import Sidebar from '../../molecules/sidebar/Sidebar'
 
 const TableProductsAdminPage = () => {
   return (
     <>
-    <Sidebar />
    <TableProducts />
    </>
   )

552ad6c4ed1ca23e4ff3b5328e527f5a6f5175c4
Desiree <desireesanchezdev@gmail.com>
2024-04-19 09:08:36 +0200

    Create NewEventAdminPage

---
+++ src/components/pages/newEventAdminPage/NewEventAdminPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import EventFormAdmin from '../../molecules/eventFormAdmin/EventFormAdmin'
+
+const NewEventAdminPage = () => {
+  return (
+    <EventFormAdmin/>
+  )
+}
+
+export default NewEventAdminPage

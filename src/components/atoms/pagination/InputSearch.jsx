a4b351e64824ae1e58c0d2a80be65c9c58cb85cc
Claudia González <claudia.glezg@gmail.com>
2024-04-19 18:50:06 +0200

    Change placeholder

--- src/components/atoms/pagination/InputSearch.jsx
+++ src/components/atoms/pagination/InputSearch.jsx
@@ -27,7 +27,7 @@ const InputSearch = ({ data, onDataFiltered }) => {
           type="text"
           id="table-search"
           className="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Búsqueda por producto"
+          placeholder="Búsqueda"
           value={searchTerm}
           onChange={handleSearchChange}
         />

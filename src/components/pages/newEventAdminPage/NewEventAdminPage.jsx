b0de6e1494a6dbcfd657e19e4c8c770ce993f459
Desiree <desireesanchezdev@gmail.com>
2024-04-19 10:57:37 +0200

    Import Sidebar on NewEventAdminPage

--- src/components/pages/newEventAdminPage/NewEventAdminPage.jsx
+++ src/components/pages/newEventAdminPage/NewEventAdminPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import EventFormAdmin from '../../molecules/eventFormAdmin/EventFormAdmin'
+import Sidebar from '../../molecules/sidebar/Sidebar'
 
 const NewEventAdminPage = () => {
   return (
+    <>
+    <Sidebar/>
     <EventFormAdmin/>
+    </>
   )
 }
 

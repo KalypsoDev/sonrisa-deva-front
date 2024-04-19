1387617f9454a1cf1ee4bd469b1c5cf02d5b7837
Claudia González <claudia.glezg@gmail.com>
2024-04-19 17:57:09 +0200

    Change margins to the table

--- src/components/molecules/tableProducts/TableProducts.jsx
+++ src/components/molecules/tableProducts/TableProducts.jsx
@@ -3,30 +3,31 @@ import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
 
 const TableProducts = () => {
     return (
-        <div className="w-3/4 mx-auto relative overflow-x-auto shadow-md sm:rounded-lg font-montserratRegular">
-            <table className="w-full text-sm text-center rtl:text-right text-darkGrey dark:text-gray-400">
+        <div className="overflow-x-auto shadow-md sm:rounded-lg font-montserratRegular">
+
+            <table className="w-full max-w-full text-sm text-center rtl:text-right text-darkGrey dark:text-gray-400">
                 <thead className="text-xs text-white bg-primaryLila dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" className="px-16 py-3">
+                        <th scope="col" className="px-8 py-3">
                             Acciones
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-8 py-3">
                             Nombre Producto
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-8 py-3">
                             Imagen
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-8 py-3">
                             Cantidad
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-8 py-3">
                             Precio Ud.
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <td className="px-4 py-2 flex items-center justify-center space-x-4 mt-16">
+                        <td className="px-4 py-2 flex items-center justify-center space-x-4 mt-10">
                             <a href="#" className="text-xl text-greenPen dark:text-red-500 flex items-center justify-center"><FaPencilAlt /></a>
                             <a href="#" className="text-xl text-redBin dark:text-red-500 flex items-center justify-center"><FaTrashAlt /></a>
                         </td>
@@ -36,7 +37,7 @@ const TableProducts = () => {
                         <td className="py-2">
                             <img
                                 src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/principito.jpg"
-                                className="w-12 md:w-32 max-w-full max-h-full mx-auto rounded-lg"
+                                className="object-cover w-24 h-24 md:w-32 max-w-full max-h-full mx-auto rounded-lg"
                                 alt="Apple Watch"
                             />
                         </td>
@@ -50,7 +51,7 @@ const TableProducts = () => {
                         </td>
                     </tr>
                     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <td className="px-4 py-2 flex items-center justify-center space-x-4 mt-16">
+                        <td className="px-4 py-2 flex items-center justify-center space-x-4 mt-10">
                             <a href="#" className="text-xl text-greenPen dark:text-red-500 flex items-center justify-center"><FaPencilAlt /></a>
                             <a href="#" className="text-xl text-redBin dark:text-red-500 flex items-center justify-center"><FaTrashAlt /></a>
                         </td>
@@ -60,7 +61,7 @@ const TableProducts = () => {
                         <td className="py-2">
                             <img
                                 src="https://monpetitparapluie.s3.eu-west-3.amazonaws.com/upload/principito.jpg"
-                                className="w-12 md:w-32 max-w-full max-h-full mx-auto rounded-lg"
+                                className="object-cover w-24 h-24 md:w-32 max-w-full max-h-full mx-auto rounded-lg"
                                 alt="Apple Watch"
                             />
                         </td>

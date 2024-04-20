cde208bd9309b89f93dcafeffea110f7437ac0f3
Claudia González <claudia.glezg@gmail.com>
2024-04-20 18:53:43 +0200

    Add logic to visualize 2 events per section

--- src/components/pages/eventPage/EventPage.jsx
+++ src/components/pages/eventPage/EventPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardEvent from '../../molecules/cardEvent/CardEvent';
 import { NavbarMolecule } from '../../molecules/navbar/NavbarMolecule';
 import { Footer } from '../../molecules/footer/Footer';
@@ -9,31 +9,88 @@ const EventPage = () => {
     { id: 1, title: 'Evento 1', date: '', hour: '12:00', location: 'Avilés', collection: '', imageSrc: 'https://picsum.photos/200/300' },
     { id: 2, title: 'Evento 2', date: '2024-05-17', hour: '12:00', location: 'León', collection: '', imageSrc: 'https://picsum.photos/200/300' },
     { id: 3, title: 'Evento 3', date: '2024-01-20', hour: '12:00', location: 'Oviedo', collection: '1100', imageSrc: 'https://picsum.photos/200/300' },
-    { id: 4, title: 'Evento 4', date: '2024-02-14', hour: '12:00', location: 'Gijón', collection: '2000', imageSrc: 'https://picsum.photos/200/300' }
+    { id: 4, title: 'Evento 4', date: '2024-02-14', hour: '12:00', location: 'Gijón', collection: '2000', imageSrc: 'https://picsum.photos/200/300' },
+    { id: 5, title: 'Evento 5', date: '2024-02-15', hour: '12:00', location: 'Gijón', collection: '2000', imageSrc: 'https://picsum.photos/200/300' },
+    { id: 6, title: 'Evento 6', date: '', hour: '12:00', location: 'Avilés', collection: '', imageSrc: 'https://picsum.photos/200/300' },
+    { id: 7, title: 'Evento 7', date: '2024-03-17', hour: '12:00', location: 'León', collection: '700', imageSrc: 'https://picsum.photos/200/300' },
+    { id: 8, title: 'Evento 8', date: '2024-10-20', hour: '12:00', location: 'Oviedo', collection: '', imageSrc: 'https://picsum.photos/200/300' },
+    { id: 9, title: 'Evento 9', date: '2024-02-22', hour: '12:00', location: 'Gijón', collection: '2000', imageSrc: 'https://picsum.photos/200/300' },
+    { id: 10, title: 'Evento 10', date: '2024-07-23', hour: '12:00', location: 'Gijón', collection: '', imageSrc: 'https://picsum.photos/200/300' }
   ];
 
-  const today = new Date(); // Fecha actual
+  const [visibleUpcomingEvents, setVisibleUpcomingEvents] = useState([]);
+  const [visiblePastEvents, setVisiblePastEvents] = useState([]);
+  const [upcomingIndex, setUpcomingIndex] = useState(0);
+  const [pastIndex, setPastIndex] = useState(0);
 
-  // Filtrar eventos según si son próximos o anteriores a la fecha actual
-  const upcomingEvents = events.filter(event => {
-    // Verificar si la fecha está vacía o es una fecha válida
-    if (!event.date) {
-      return true; // Si la fecha está vacía, se considera "por determinar"
-    } else {
-      const eventDate = new Date(event.date);
-      return eventDate >= today;
-    }
-  });
+  useEffect(() => {
+    const today = new Date();
 
-  const pastEvents = events.filter(event => {
-    // Verificar si la fecha está vacía o es una fecha válida
-    if (!event.date) {
-      return false; // Si la fecha está vacía, no se considera en eventos pasados
-    } else {
-      const eventDate = new Date(event.date);
-      return eventDate < today;
+    // Filtrar eventos próximos
+    const upcoming = events.filter(event => {
+      if (!event.date) {
+        return true; // Evento sin fecha (por determinar)
+      } else {
+        const eventDate = new Date(event.date);
+        return eventDate >= today;
+      }
+    });
+
+    // Filtrar eventos pasados
+    const past = events.filter(event => {
+      if (!event.date) {
+        return false; // Evento sin fecha (no considerado en eventos pasados)
+      } else {
+        const eventDate = new Date(event.date);
+        return eventDate < today;
+      }
+    });
+
+    setVisibleUpcomingEvents(upcoming);
+    setVisiblePastEvents(past);
+  }, [events]); // Actualizar cuando cambian los eventos
+
+  const renderUpcomingEvents = () => {
+    const slicedUpcoming = visibleUpcomingEvents.slice(upcomingIndex, upcomingIndex + 2);
+    return slicedUpcoming.map(event => (
+      <div key={event.id} className="w-full md:w-1/2 px-4"> 
+        <CardEvent event={event} />
+      </div>
+    ));
+  };
+
+  const renderPastEvents = () => {
+    const slicedPast = visiblePastEvents.slice(pastIndex, pastIndex + 2);
+    return slicedPast.map(event => (
+      <div key={event.id} className="w-full md:w-1/2 px-4"> 
+        <CardEvent event={event} />
+      </div>
+    ));
+  };
+
+  const handleUpcomingNext = () => {
+    if (upcomingIndex < visibleUpcomingEvents.length - 2) {
+      setUpcomingIndex(prevIndex => prevIndex + 2);
     }
-  });
+  };
+
+  const handleUpcomingPrev = () => {
+    if (upcomingIndex > 0) {
+      setUpcomingIndex(prevIndex => prevIndex - 2);
+    }
+  };
+
+  const handlePastNext = () => {
+    if (pastIndex < visiblePastEvents.length - 2) {
+      setPastIndex(prevIndex => prevIndex + 2);
+    }
+  };
+
+  const handlePastPrev = () => {
+    if (pastIndex > 0) {
+      setPastIndex(prevIndex => prevIndex - 2);
+    }
+  };
 
   return (
     <>
@@ -44,32 +101,26 @@ const EventPage = () => {
           <p className="text-sm md:text-lg font-montserratRegular text-primaryBlue text-center mb-8">Si quieres realizar un evento solidario en apoyo a nuestra causa</p>
         </div>
         
-        {/* Sección de Próximos Eventos */}
         <div className="flex justify-start items-center mb-4 pl-4 ml-4">
-          <p className="text-2xl text-primaryBlue font-bold">Próximos Eventos</p>
-          <FaCircleChevronLeft className="text-primaryBlue text-3xl ml-2" />
-          <FaCircleChevronRight className="text-primaryBlue text-3xl ml-2" />
+          <p className="text-2xl text-primaryBlue font-montserratBold">Próximos Eventos</p>
+          <div className="flex items-center">
+            <FaCircleChevronLeft className="text-primaryBlue text-3xl ml-2 cursor-pointer" onClick={handleUpcomingPrev} />
+            <FaCircleChevronRight className="text-primaryBlue text-3xl ml-2 cursor-pointer" onClick={handleUpcomingNext} />
+          </div>
         </div>
         <div className="flex justify-center w-full mb-8">
-          {upcomingEvents.map(event => (
-            <div key={event.id} className="w-full md:w-1/2 px-4"> 
-              <CardEvent event={event} />
-            </div>
-          ))}
+          {renderUpcomingEvents()}
         </div>
 
-        {/* Sección de Eventos Anteriores */}
         <div className="flex justify-start items-center mb-4 pl-4 ml-4">
-          <p className="text-2xl text-primaryBlue font-bold">Eventos Anteriores</p>
-          <FaCircleChevronLeft className="text-primaryBlue text-3xl ml-2" />
-          <FaCircleChevronRight className="text-primaryBlue text-3xl ml-2" />
+          <p className="text-2xl text-primaryBlue font-montserratBold">Eventos Anteriores</p>
+          <div className="flex items-center">
+            <FaCircleChevronLeft className="text-primaryBlue text-3xl ml-2 cursor-pointer" onClick={handlePastPrev} />
+            <FaCircleChevronRight className="text-primaryBlue text-3xl ml-2 cursor-pointer" onClick={handlePastNext} />
+          </div>
         </div>
         <div className="flex justify-center w-full">
-          {pastEvents.map(event => (
-            <div key={event.id} className="w-full md:w-1/2 px-4"> 
-              <CardEvent event={event} />
-            </div>
-          ))}
+          {renderPastEvents()}
         </div>
       </section>
       <Footer />
@@ -78,11 +129,4 @@ const EventPage = () => {
 };
 
 export default EventPage;
-
-
-
-
-
-
-
 

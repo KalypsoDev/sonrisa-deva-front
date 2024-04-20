faaac349b56c242b92480b4e19b65a1476c2f17c
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-20 16:11:10 +0200

    Update styles of the card

--- src/components/molecules/angelmanSyndrome/AngelmanSyndrome.jsx
+++ src/components/molecules/angelmanSyndrome/AngelmanSyndrome.jsx
@@ -12,62 +12,72 @@ const AngelmanSyndrome = () => {
       <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row max-w-3xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-20">
         <img className="object-cover rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={deva7} alt="" />
 
-        <div className="flex flex-col justify-between p-4 leading-normal">
+          <div className="cardContent">
 
-          <h5 className="mb-2 text-2xl font-montserratBold tracking-tight text-gray-900 dark:text-white">Síndrome de Angelman</h5>
-          <div>
+            <h5 className="flex justify-center text-lg font-montserratBold  mb-2 tracking-tight text-primaryBlue dark:text-white">¿QUÉ ES EL SÍNDROME DE ANGELMAN?</h5>
+           
+            <div className="groupTherapies">
+              
+              <div className="flex p-3">
+                <div className="text-4xl text-primaryLila">
+                  <FaChartPie />
+                </div>
+                <div className="flex justify-center sm:items-center ml-6">
+                  <div>
+                    <a href="#" className="text-primaryBlue text-md hover:text-primaryBlue">
+                      <FaPlusCircle />
+                    </a>
+                  </div>
+                  <p className="text-base font-montserratRegular ml-2">Enfermedad rara (baja prevalencia)</p>
+                </div>
+              </div>
 
-            <div>
-              <div className="text-4xl text-primaryLila">
-                <FaChartPie />
+              <div className="flex p-3">
+                <div className="text-4xl text-primaryLila">
+                  <FaDna />
+                </div>
+                <div className="flex justify-center sm:items-center ml-6">
+                  <div>
+                    <a href="#" className="text-primaryBlue text-md hover:text-primaryBlue">
+                      <FaPlusCircle />
+                    </a>
+                  </div>
+                  <p className="text-base font-montserratRegular ml-2">Alteración genética</p>
+                </div>
               </div>
-              <div className="flex justify-center sm:items-center">
-                <a href="#" className=" text-2xl font-montserratRegular text-primaryBlue text-md hover:text-primaryBlue">
-                <FaPlusCircle />
-                </a>
-                <p>Enfermedad rara (baja prevalencia)</p>
+
+              <div className="flex p-3">
+                <div className="text-4xl text-primaryLila">
+                  <FaBrain />
+                </div>
+                <div className="flex justify-center sm:items-center ml-6">
+                  <div>
+                    <a href="#" className="text-primaryBlue text-md hover:text-primaryBlue">
+                      <FaPlusCircle />
+                    </a>
+                  </div>
+                  <p className="text-base font-montserratRegular ml-2">Trastorno del neurodesarrollo</p> 
+                </div>            
               </div>
-             
+
+              <div className="flex p-3">
+                <div className="text-4xl text-primaryLila">
+                  <FaPencil />
+                </div>
+                <div className="flex justify-center sm:items-center ml-6">
+                  <div>
+                    <a href="#" className="text-primaryBlue text-md hover:text-primaryBlue">
+                      <FaPlusCircle />
+                    </a>
+                  </div> 
+                  <p className="text-base font-montserratRegular ml-2">Dificultad motora, de aprendizaje y de comunicación</p>  
+                </div>         
+              </div>
+
             </div>
-
-            <div>
-              <div className="text-4xl text-primaryLila">
-                <FaDna />
-              </div>
-
-              <a href="#" className="font-montserratRegular text-primaryBlue text-md hover:text-primaryBlue">
-                <FaPlusCircle />
-                <p>Alteración genética</p>
-              </a>
-            </div>
-
-            <div>
-              <div className="text-4xl text-primaryLila">
-                <FaBrain />
-              </div>
-
-              <a href="#" className="font-montserratRegular text-primaryBlue text-md hover:text-primaryBlue">
-                <FaPlusCircle />
-                <p>Trastorno del neurodesarrollo</p>
-              </a>
-            </div>
-
-            <div>
-              <div className="text-4xl text-primaryLila">
-                <FaPencil />
-              </div>
-
-              <a href="#" className="font-montserratRegular text-primaryBlue text-md hover:text-primaryBlue">
-                <FaPlusCircle />
-                <p>Dificultad motora, de aprendizaje y de comunicación</p>
-              </a>
-            </div>
-
           </div>
-
         </div>
-      </div>
-    </>
+     </>
   )
 }
 

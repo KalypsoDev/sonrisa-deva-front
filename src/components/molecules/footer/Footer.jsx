04088141c415ffb84e02f004eb7a9c778d2897cb
KalypsoDev <antunezangela89@gmail.com>
2024-04-20 22:47:54 +0200

    WIP with the structure

--- src/components/molecules/footer/Footer.jsx
+++ src/components/molecules/footer/Footer.jsx
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom"
 const Footer = () => {
   return (
     <footer className="bg-white relative pt-4 drop-shadow-[0-6px_6px_rgba(0,130,202,0.3)]">
+      <div className="flex flex-row justify-center mx-auto">
+          <div className="flex justify-center">
             <div className="flex flex-col text-center">
               <Link to="/aviso-legal" className="font-montserratRegular text-darkGrey hover:text-primaryBlue mt-2">Aviso legal</Link>
               <Link to="/politicas-privacidad" className="font-montserratRegular text-darkGrey hover:text-primaryBlue mt-2">Políticas de privacidad</Link>
@@ -16,8 +18,8 @@ const Footer = () => {
             </div>
             <p className="font-montserratRegular text-darkGrey text-md">Copyright © 2024 La Sonrisa de Deva</p>
           </div>
-          <div className="flex flex-col justify-center sm:justify-end mt-4 sm:mt-0">
-            <div className="flex items-center mt-4 sm:mt-0">
+          <div className="flex flex-col justify-center mt-4 sm:mt-0">
+            <div className="flex justify-center items-center mt-4 sm:mt-0">
                 <a href="mailto:lasonrisadedeva@gmail.com">
                 <FaRegEnvelope className="text-lg text-darkGrey hover:text-primaryBlue" />
                 </a>
@@ -28,11 +30,8 @@ const Footer = () => {
                 <FaInstagram className="text-lg text-darkGrey hover:text-primaryBlue" />
                 </a>
             </div>
-            <div className="flex flex-col items-center mt-4 sm:mt-0">
-                <Link to="/inicio-sesion-admin" className="font-montserratRegular text-darkGrey hover:text-primaryBlue mt-2">Acceso Administración</Link>
-            </div>
+            <Link to="/inicio-sesion-admin" className="font-montserratRegular text-darkGrey hover:text-primaryBlue mt-2">Acceso Administración</Link>
           </div>
-        </div>
       </div>
     </footer>
   );

c356e1f456a3667c6112303e92ca13bc7700616a
KalypsoDev <antunezangela89@gmail.com>
2024-04-21 20:08:16 +0200

    Make footer responsive

--- src/components/molecules/footer/Footer.jsx
+++ src/components/molecules/footer/Footer.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom"
 const Footer = () => {
   return (
     <footer className="bg-white relative pt-4 pb-4 drop-shadow-[0-6px_6px_rgba(0,130,202,0.3)]">
-      <div className="container flex flex-row justify-between max-w-screen-xl mx-auto">
-        <div className="flex flex-col text-center justify-evenly justify-items-center">
+      <div className="container flex flex-wrap flex-row justify-center md:justify-between max-w-screen-xl mx-auto">
+        <div className="flex flex-col text-center justify-center mb-2 md:mb-0 md:justify-evenly justify-items-center">
           <Link to="/aviso-legal" className="font-montserratRegular text-darkGrey hover:text-primaryBlue mt-2">Aviso legal</Link>
           <Link to="/politicas-privacidad" className="font-montserratRegular text-darkGrey hover:text-primaryBlue mt-2">Política de privacidad</Link>
         </div>
@@ -17,7 +17,7 @@ const Footer = () => {
           <p className="font-montserratRegular text-darkGrey text-md">Copyright © 2024 La Sonrisa de Deva</p>
         </div>
         <div className="flex flex-col text-center justify-evenly justify-items-center mt-4">
-          <div className="flex justify-evenly items-center">
+          <div className="flex justify-evenly items-center mb-2 md:mb-0">
               <a href="mailto:lasonrisadedeva@gmail.com">
               <FaRegEnvelope className="text-3xl text-darkGrey hover:text-primaryBlue" />
               </a>

5b5ada556c0e30d25830225936cd1e8e15a5967e
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-21 11:21:31 +0200

    Styles icons, fonts and color of LegendModal component

--- src/components/atoms/legendModal/LegendModal.jsx
+++ src/components/atoms/legendModal/LegendModal.jsx
@@ -10,29 +10,38 @@ const LegendModal = ({ isOpen, handleClose }) => {
             {isOpen && (
                 <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
                     <div className="bg-white w-full max-w-md p-6 rounded-lg">
-                        {/* Modal header */}
+
                         <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                            <h3 className="text-lg font-montserratBold text-primaryBlue dark:text-white">
                                 Leyenda de estados
                             </h3>
                             <button
-                                className="text-gray-600 hover:text-gray-800"
+                                className="text-primaryBlue text-2xl hover:text-gray-800"
                                 onClick={handleClose}
                             >
                                 <IoIosCloseCircle />
                             </button>
                         </div>
-                        {/* Modal body */}
+                        
                         <div className="p-4 md:p-5 space-y-4">
-                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            <FaBoxOpen /> En preparación
-                            </p>
-                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            <FaPaperPlane /> Enviado
-                            </p>
-                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            <FcCancel /> Cancelado
-                            </p>
+                           <div className="flex items-center">
+                               <FaBoxOpen className="text-mustardBox text-2xl mr-3" />
+                                <p className="text-base font-montserratBold leading-relaxed text-darkGrey dark:text-gray-400">
+                                    En preparación
+                                </p>
+                            </div>
+                             <div className="flex items-center">
+                                 <FaPaperPlane className="text-primaryBlue text-2xl mr-3" />
+                                <p className="text-base font-montserratBold leading-relaxed text-darkGreydark:text-gray-400">
+                                   Enviado
+                                </p>
+                            </div>
+                             <div className="flex items-center">
+                                 <FcCancel className="text-redBin text-2xl mr-3" />
+                                <p className="text-base font-montserratBold leading-relaxed text-darkGreydark:text-gray-400">
+                                   Cancelado
+                                </p>
+                           </div>
                         </div>
 
                     </div>
@@ -43,5 +52,3 @@ const LegendModal = ({ isOpen, handleClose }) => {
 };
 
 export default LegendModal;
-
-

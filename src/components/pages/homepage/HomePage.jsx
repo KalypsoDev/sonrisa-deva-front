17b3953b5ce76e2957eb0330c7b7ae046e23702e
KalypsoDev <antunezangela89@gmail.com>
2024-04-21 19:23:22 +0200

    WIP in CardWithIcon structure

--- src/components/pages/homepage/HomePage.jsx
+++ src/components/pages/homepage/HomePage.jsx
@@ -1,5 +1,6 @@
 import  {NavbarMolecule}  from "../../molecules/navbar/NavbarMolecule";
 import  {Footer}  from "../../molecules/footer/Footer";
+import CardWithIcon from "../../molecules/cardWithIcon/CardWithIcon";
 
 
 export function HomePage() {
@@ -7,6 +8,7 @@ export function HomePage() {
     <>
         <NavbarMolecule></NavbarMolecule>
         <h1 className="font-montserratBold">Ésta es la home page</h1>
+        <CardWithIcon></CardWithIcon>
         <Footer></Footer>
     </>
   );

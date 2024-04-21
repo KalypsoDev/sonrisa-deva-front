f04e43884d45981d4611ec036ab5c471bc163c86
Desiree <desireesanchezdev@gmail.com>
2024-04-21 19:23:27 +0200

    Add structure on AssociationCard

--- src/components/molecules/associationCard/AssociationCard.jsx
+++ src/components/molecules/associationCard/AssociationCard.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const AssociationCard = () => {
-  return (
-    <div>AssociationCard</div>
-  )
+const AssociationCard = ({ iconPosition, icon, title, description, additionalText }) => {
+    return (
+        <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow-md flex flex-col font-montserratRegular">
+            <div className="flex items-center mb-3 flex-col md:flex-row">
+                {iconPosition === 'left' && (
+                    <div className='border-b p-2 mr-3'>
+                        {icon}
+                    </div>
+                )}
+                <h5 className="text-xl font-montserratBold tracking-tight text-darkGrey">{title}</h5>
+                {iconPosition === 'right' && (
+                    <div className='border-b p-2 ml-3'>
+                        {icon}
+                    </div>
+                )}
+            </div>
+            <div>
+                <p className=" text-darkGrey">{description}</p>
+                {additionalText && (
+                    <p className="mt-2 text-base text-darkGrey">{additionalText}</p>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default AssociationCard

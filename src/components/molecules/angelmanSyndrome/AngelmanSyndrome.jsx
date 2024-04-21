dca51810e63cda96a0063283ca99ccd152fa84a9
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-21 14:52:28 +0200

    Modify AngelmanSyndrome molecule to receive the parameters

--- src/components/molecules/angelmanSyndrome/AngelmanSyndrome.jsx
+++ src/components/molecules/angelmanSyndrome/AngelmanSyndrome.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import deva7 from "../../../assets/img/deva7.png";
 import { FaPlusCircle } from "react-icons/fa";
 import { FaChartPie } from "react-icons/fa6";
@@ -6,7 +6,15 @@ import { FaDna } from "react-icons/fa6";
 import { FaBrain } from "react-icons/fa6";
 import { FaPencil } from "react-icons/fa6";
 
+import AngelmanSyndromeModal from '../../atoms/angelmanSyndromeModal/AngelmanSyndromeModal';
+
 const AngelmanSyndrome = () => {
+  const [selectedSymptom, setSelectedSymptom] = useState(null);
+
+  const handlesSymptomSelect = (modalTitle, modalDescription) => {
+    setSelectedSymptom({ modalTitle, modalDescription });
+  };
+  
   return (
     <>
       <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row max-w-3xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-20">
@@ -18,10 +26,8 @@ const AngelmanSyndrome = () => {
            
             <div className="groupTherapies">
               
-              <div className="flex p-3">
-                <div className="text-4xl text-primaryLila">
-                  <FaChartPie />
-                </div>
+            <div className="flex p-3" onClick={() => handlesSymptomSelect('Enfermedad rara (baja prevalencia)', 'Por su baja prevalencia, el Síndrome de Angelman está considerado como enfermedad rara (su prevalencia es de 1/15000 nacimientos).')}>
+                <div className="text-4xl text-primaryLila"><FaChartPie /></div>
                 <div className="flex justify-center sm:items-center ml-6">
                   <div>
                     <a href="#" className="text-primaryBlue text-md hover:text-primaryBlue">
@@ -32,7 +38,7 @@ const AngelmanSyndrome = () => {
                 </div>
               </div>
 
-              <div className="flex p-3">
+              <div className="flex p-3" onClick={() => handlesSymptomSelect('Alteración genética', '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.')}>
                 <div className="text-4xl text-primaryLila">
                   <FaDna />
                 </div>
@@ -46,7 +52,7 @@ const AngelmanSyndrome = () => {
                 </div>
               </div>
 
-              <div className="flex p-3">
+              <div className="flex p-3" onClick={() => handlesSymptomSelect('Trastorno del neurodesarrollo', '3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.')}>
                 <div className="text-4xl text-primaryLila">
                   <FaBrain />
                 </div>
@@ -60,7 +66,7 @@ const AngelmanSyndrome = () => {
                 </div>            
               </div>
 
-              <div className="flex p-3">
+              <div className="flex p-3" onClick={() => handlesSymptomSelect('Dificultad motora, de aprendizaje y de comunicación', '4. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor')}>
                 <div className="text-4xl text-primaryLila">
                   <FaPencil />
                 </div>
@@ -77,6 +83,7 @@ const AngelmanSyndrome = () => {
             </div>
           </div>
         </div>
+        <AngelmanSyndromeModal therapy={selectedSymptom} handleClose={() => setSelectedSymptom(null)} />
      </>
   )
 }

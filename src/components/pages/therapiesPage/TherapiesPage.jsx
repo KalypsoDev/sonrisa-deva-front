554d5a245a1ad39fb3fc48fa32fc5c67539dd890
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-21 16:57:21 +0200

    Add TherapiesCardPage folder and file

---
+++ src/components/pages/therapiesPage/TherapiesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TherapiesCard from '../../molecules/therapies/TherapiesCard'
+
+
+const TherapiesPage = () => {
+  return (
+    <div>
+        <TherapiesCard />
+    </div>
+  )
+}
+
+export default TherapiesPage

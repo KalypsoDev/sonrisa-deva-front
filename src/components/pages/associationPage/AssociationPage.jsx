e780dff6fb31b9d4ba22e4774dae29293eecade6
Desiree <desireesanchezdev@gmail.com>
2024-04-22 21:16:12 +0200

    Edit styles on AsociationPage

--- src/components/pages/associationPage/AssociationPage.jsx
+++ src/components/pages/associationPage/AssociationPage.jsx
@@ -11,7 +11,8 @@ const AssociationPage = () => {
   return (
     <>
       <Navbar />
-      <section className='bg-backgroundBlue pt-7 relative'>
+      <section className='bg-backgroundBlue relative'>
+        <h1 className='text-primaryBlue text-center font-montserratBold text-xl pt-5 md:text-2xl'>LA ASOCIACION LA SONRISA DE DEVA</h1>
         <div className="flex justify-items-center">
           <div className="flex flex-wrap md:flex-row md:justify-center">
             <div className="w-full md:w-1/2 flex justify-end relative">
@@ -22,41 +23,44 @@ const AssociationPage = () => {
                 additionalText="Pretende, asimismo, ser un espacio de acogida para familias que se encuentren en la misma situación, y una red de apoyo sólido y directo a la investigación del síndrome de Angelman."
               />
             </div>
-            <div className="w-full md:w-1/2 flex justify-start relative mb-28 md:mb-0">
+            <div className="w-full md:w-1/2 flex justify-start relative mb-56 md:mb-0 text-end">
+              <AssociationCard
+                icon={<FaBullseye className='text-6xl text-primaryBlue' />}
+                title="Objetivos"
+                description={[
+                  <span><strong>1.</strong> <strong>Visibilizar</strong> y concienciar sobre el síndrome de Angelman</span>,
+                  <span><strong>2.</strong> Recaudar <strong>fondos para investigar</strong> su tratamiento</span>,
+                  <span><strong>3.</strong> <strong>Financiar</strong> el acceso a <strong> terapias y/o aparatos ortoprotésicos</strong> que ayuden a mejorar la calidad de vida de Deva y/o otros niños en su situación.</span>,
+                  <span><strong>4.</strong> Fomentar la <strong>inclusión</strong> de personas con diversidad funcional en todos los ámbitos de la sociedad.</span>
+                ]}
+              />
+
+            </div>
+            <img src="src\assets\img\asociacion-deva.png" alt="Imagen Deva" className="border-4 border-primaryBlue rounded-full h-52 w-52 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10" />
+            <div className="w-full md:w-1/2 flex justify-end relative mt-28 md:mt-0 text-center">
               <AssociationCard
                 icon={<FaHandHoldingHeart className='text-6xl text-primaryBlue' />}
                 title="Valores"
-                description="- Participación 
-                       - Respeto
-                       - Inclusión 
-                       - Transparencia 
-                       - Lucha
-                       - Resiliencia
-                       - Ayuda"
+                description={[
+                  "- Participación",
+                  "- Respeto",
+                  "- Inclusión",
+                  "- Transparencia",
+                  "- Lucha",
+                  "- Resiliencia",
+                  "- Ayuda"
+                ]}
               />
             </div>
-            <img src="src\assets\isotipo.jpg" alt="Isologo" className="rounded-full h-48 w-48 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10" />
-            <div className="w-full md:w-1/2 flex justify-end relative mt-28 md:mt-0 text-center">
-              <AssociationCard
-                icon={<FaBullseye className='text-6xl text-primaryBlue' />}
-                title="Valores"
-                description="- Participación 
-                       - Respeto
-                       - Inclusión 
-                       - Transparencia 
-                       - Lucha
-                       - Resiliencia
-                       - Ayuda"
-              />
-            </div>
-            <div className="w-full md:w-1/2 flex justify-start relative">
+            <div className="w-full md:w-1/2 flex justify-start relative text-center">
               <AssociationCard
                 icon={<FaCalendarAlt className='text-6xl text-primaryBlue' />}
                 title="Actividades"
-                description="
-              1. Difusión de las características del síndrome, evolución de la investigación científica y peculiaridades de Deva en redes sociales y medios de comunicación.
-              2. Organización de eventos solidarios.
-              3. Cooperación y colaboración con diversas entidades sin ánimo de lucro en el cumplimiento de sus fines (Fundación Fast España, Asociación Síndrome de Angelman España, y otras con fines similares y principios compatibles con los de LA SONRISA DE DEVA)."
+                description={[
+                  <span><strong>1.</strong> <strong>Difusión</strong> de las características del síndrome, evolución de la investigación científica y peculiaridades de Deva en redes sociales y medios de comunicación.</span>,
+                  <span><strong>2.</strong> Organización de <strong>eventos solidarios.</strong></span>,
+                  <span><strong>3.</strong> <strong>Cooperación y colaboración</strong> con diversas entidades sin ánimo de lucro en el cumplimiento de sus fines (Fundación Fast España, Asociación Síndrome de Angelman España, y otras con fines similares y principios compatibles con los de LA SONRISA DE DEVA).</span>
+                ]}
               />
             </div>
           </div>

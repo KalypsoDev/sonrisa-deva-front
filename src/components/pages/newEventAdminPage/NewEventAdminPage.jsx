228e17dc99e3fc488778d1cb510d4b938f4360a8
Desiree <desireesanchezdev@gmail.com>
2024-04-22 19:55:31 +0200

    Add subimitButtonText and edit styles on newEventAdminPage

--- src/components/pages/newEventAdminPage/NewEventAdminPage.jsx
+++ src/components/pages/newEventAdminPage/NewEventAdminPage.jsx
@@ -4,29 +4,33 @@ import Sidebar from '../../molecules/sidebar/Sidebar';
 import FetchApi from '../../../services/FetchApi';
 
 const NewEventAdminPage = () => {
-    const handleSubmit = async (formData) => {
-        try {
-            const newEvent = await FetchApi.createEvent(formData);
-            console.log('Evento creado exitosamente:', newEvent);
-        } catch (error) {
-            console.error('Error al crear el evento:', error);
-        }
-    };
+  const handleSubmit = async (formData) => {
+    try {
+      const newEvent = await FetchApi.createEvent(formData);
+      console.log('Evento creado exitosamente:', newEvent);
+    } catch (error) {
+      console.error('Error al crear el evento:', error);
+    }
+  };
 
-    const handleCancel = () => {
-        console.log('Creación de evento cancelada');
-    };
+  const handleCancel = () => {
+    console.log('Creación de evento cancelada');
+  };
 
-    return (
-        <>
-            <Sidebar />
-            <EventFormAdmin
-                title="AGREGAR EVENTO"
-                onSubmit={handleSubmit}
-                onCancel={handleCancel}
-            />
-        </>
-    );
+  return (
+    <>
+      <Sidebar />
+      <div className='mt-7'>
+        <h1 className='font-montserratBold text-center text-2xl'>AGENDA DE EVENTOS</h1>
+        <EventFormAdmin
+          title="PLANIFICAR UN EVENTO"
+          submitButtonText="Agregar"
+          onSubmit={handleSubmit}
+          onCancel={handleCancel}
+        />
+      </div>
+    </>
+  );
 };
 
 export default NewEventAdminPage;

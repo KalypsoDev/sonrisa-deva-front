316a3acd41dd4e0712695020af699b351cc49ff7
Desiree <desireesanchezdev@gmail.com>
2024-04-22 19:44:55 +0200

    Add api method to create Events on NewEventAdminPage

--- src/components/pages/newEventAdminPage/NewEventAdminPage.jsx
+++ src/components/pages/newEventAdminPage/NewEventAdminPage.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import EventFormAdmin from '../../molecules/eventFormAdmin/EventFormAdmin'
-import Sidebar from '../../molecules/sidebar/Sidebar'
+import React from 'react';
+import EventFormAdmin from '../../molecules/eventFormAdmin/EventFormAdmin';
+import Sidebar from '../../molecules/sidebar/Sidebar';
+import FetchApi from '../../../services/FetchApi';
 
 const NewEventAdminPage = () => {
-  return (
-    <>
-    <Sidebar/>
-    <EventFormAdmin/>
-    </>
-  )
-}
+    const handleSubmit = async (formData) => {
+        try {
+            const newEvent = await FetchApi.createEvent(formData);
+            console.log('Evento creado exitosamente:', newEvent);
+        } catch (error) {
+            console.error('Error al crear el evento:', error);
+        }
+    };
 
-export default NewEventAdminPage
+    const handleCancel = () => {
+        console.log('Creación de evento cancelada');
+    };
+
+    return (
+        <>
+            <Sidebar />
+            <EventFormAdmin
+                title="AGREGAR EVENTO"
+                onSubmit={handleSubmit}
+                onCancel={handleCancel}
+            />
+        </>
+    );
+};
+
+export default NewEventAdminPage;

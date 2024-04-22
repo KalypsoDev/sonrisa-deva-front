d08b8bd0fe41c7e45951999275837178025ecc88
Desiree <desireesanchezdev@gmail.com>
2024-04-22 15:15:24 +0200

    Edit styles on AssociationCard

--- src/components/molecules/associationCard/AssociationCard.jsx
+++ src/components/molecules/associationCard/AssociationCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AssociationCard = ({ icon, title, description, additionalText }) => {
     return (
-        <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow-md flex flex-col font-montserratRegular mb-5">
+        <div className="max-w-xl bg-white border border-gray-200 rounded-lg shadow-md flex flex-col font-montserratRegular m-3 md:m-8">
             <div className="flex items-center justify-center mb-3 flex-col md:flex-row">
                 <div className='border-b p-2 mr-3'>
                     {icon}
@@ -10,12 +10,12 @@ const AssociationCard = ({ icon, title, description, additionalText }) => {
                 <h5 className="text-xl font-montserratBold tracking-tight text-darkGrey">{title}</h5>
             </div>
             <div>
-                <p className="text-darkGrey">{description}</p>
+                <ul className="text-darkGrey">{description}</ul>
                 <p className="mt-2 text-base text-darkGrey">{additionalText}</p>
             </div>
         </div>
-
     )
 }
 
-export default AssociationCard
+export default AssociationCard;
+

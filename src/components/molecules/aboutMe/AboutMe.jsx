4b832f10edafa97efb61ff688f5b2af17d47751e
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-23 22:56:52 +0200

    Modify AboutMe card structure

--- src/components/molecules/aboutMe/AboutMe.jsx
+++ src/components/molecules/aboutMe/AboutMe.jsx
@@ -9,29 +9,23 @@ const AboutMe = () => {
 
     return (
         <>
-            <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row max-w-4xl hover:bg-gray-100 m-20 mx-auto" style={shadowStyle}>
+            <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row max-w-3xl hover:bg-gray-100 m-20 mx-auto" style={shadowStyle}>
+                <img className="w-full object-cover rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={deva8} alt="Imagen Deva" />
+                <div className="cardContent">
 
-                <div className="flex flex-col items-center w-fit rounded-t-lg md:rounded-l-lg h-72 md:h-auto md:w-80 md:flex-none md:justify-center">
-                    <img className="rounded-lg w-full md:h-auto" src={deva8} alt="Deva Therapies" />
-                </div>
+                    <h1 className="flex justify-center text-center text-lg font-montserratBold mt-4 mb-2 tracking-tight text-primaryBlue">ACERCA DE MÍ</h1>
 
-                <div className="flex flex-col w-full justify-evenly md:pb-4 leading-normal">
-                    <div className="text-center mb-14">
-                        <h1 className="text-2xl font-montserratBold text-primaryBlue">ACERCA DE Mí</h1>
-                    </div>
+                    <div className="flex groupTherapies">
 
-                    <div className="md:flex flex-col-2 w-full justify-center">
-                        <div className="text-darkGrey mb-1 p-2">
+                        <div className="flex p-3">
                             <p>¡Hola! Me llamo Deva y nací un 27 de agosto de 2018. Solo un año más tarde me diagnosticaron síndrome de Angelman. Este diagnóstico, acompañado de un alto porcentaje de discapacidad y el grado más alto de dependencia cambiaría todos los planes que mi familia tenía para mí.</p>
-                        </div>
-                        <div className="text-darkGrey mb-2 p-2">
+
                             <p>Desde ese mismo día trabajo muy duro en distintas terapias para lograr derribar las barreras impuestas por un capricho genético.</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
-
+         </>
     )
 }
 

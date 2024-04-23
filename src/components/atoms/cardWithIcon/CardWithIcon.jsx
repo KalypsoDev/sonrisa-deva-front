9ff3583b91f1b1331f782d8ce244138297e00baf
KalypsoDev <antunezangela89@gmail.com>
2024-04-23 14:27:00 +0200

    Use boolean to make the card work better

--- src/components/atoms/cardWithIcon/CardWithIcon.jsx
+++ src/components/atoms/cardWithIcon/CardWithIcon.jsx
@@ -15,9 +15,7 @@ const CardWithIcon = ({
 
       <div className={`max-w-sm p-6 bg-${backgroundColor} border border-gray-200 rounded-lg`} style={shadowStyle}>
             <h3 className={`text-base p-4 md:text-lg font-montserratBold text-center mb-4 text-${color}`}>{title}</h3>
-            <div className={`p-6 text-${iconColor}`}>
-              <Icon />
-            </div>
+            {Icon && <Icon className={`text-${iconColor} text-6xl mx-auto`} />}
             {subtitle !== undefined && <p className={`text-base p-4 md:text-lg font-montserratRegular text-center mb-4 text-${color}`}>{subtitle}</p>}
       </div>
   );

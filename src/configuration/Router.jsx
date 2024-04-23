9b22dda5dc7962dfb0d1dff0c6ba9b5b9fff55a5
KalypsoDev <antunezangela89@gmail.com>
2024-04-23 22:28:19 +0200

    Add DonationPage (component, route)

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -18,6 +18,7 @@ import AssociationPage from '../components/pages/associationPage/AssociationPage
 import WhoAmIPage from '../components/pages/whoAmIPage/WhoAmIPage';
 import AboutDevaPage from '../components/pages/aboutDevaPage/AboutDevaPage';
 import CooperatePage from '../components/pages/cooperatePage/CooperatePage';
+import DonationPage from '../components/pages/donationPage/DonationPage';
 
 const Router = () => {
     return (
@@ -42,6 +43,7 @@ const Router = () => {
                   <Route path='/quien-soy' element={<WhoAmIPage/>}></Route>
                   <Route path='/sobre-deva' element={<AboutDevaPage/>}></Route>
                   <Route path='/colabora' element={<CooperatePage/>}></Route>
+                  <Route path='/donacion' element={<DonationPage/>}></Route>
           </Routes>
       </BrowserRouter>
     )

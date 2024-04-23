d34d4f1f01e384e81d3a2ad77f81c47bc98360bd
KalypsoDev <antunezangela89@gmail.com>
2024-04-23 15:32:13 +0200

    Make CardWithIcon work in WhoAmIPage

--- src/components/pages/whoAmIPage/WhoAmIPage.jsx
+++ src/components/pages/whoAmIPage/WhoAmIPage.jsx
@@ -1,35 +1,42 @@
 import  Navbar  from "../../molecules/navbar/Navbar";
 import  Footer  from "../../molecules/footer/Footer";
 import CardWithIcon from "../../atoms/cardWithIcon/CardWithIcon";
-import { FaRegEnvelope } from "react-icons/fa";
+import { FaHandshakeSimple } from "react-icons/fa6";
 import { PiButterflyFill } from "react-icons/pi";
 import { Link } from "react-router-dom"
 
 const WhoAmIPage = () => {
   return (
     <>
+      <div className="flex flex-col min-h-screen">
         <Navbar></Navbar>
-        <h1 className="font-montserratBold">¿Quién soy yo?</h1>
-        <div className="flex flex-col gap-4 md:flex-row mx-auto md:max-w-4xl m-6">
-          <Link to="/sobre-deva">
-            <CardWithIcon
-              backgroundColor="darkBlue"
-              color="white"
-              title="Sobre Deva"
-              Icon={PiButterflyFill}
-              iconColor="white"
-            />
-          </Link>
-          <Link to="/asociacion">
-            <CardWithIcon 
-              Icon={FaRegEnvelope}
-            />
-          </Link>
-          <CardWithIcon 
-            Icon={FaRegEnvelope}
-          />
-        </div>
+        <section className="flex-grow bg-backgroundBlue">
+          <div className="container flex flex-col items-center mx-auto p-4 h-full">
+            <h1 className="text-2xl p-4 md:text-3xl font-montserratBold text-center text-primaryBlue mb-4">¿QUIÉN SOY?</h1>
+            <div className="flex flex-col justify-center items-center gap-6 md:flex-row p-4 mx-auto md:max-w-4xl">
+              <Link to="/sobre-deva">
+                <CardWithIcon
+                  backgroundColor="darkBlue"
+                  color="white"
+                  title="Sobre Deva"
+                  Icon={PiButterflyFill}
+                  iconColor="white"
+                />
+              </Link>
+              <Link to="/asociacion">
+                <CardWithIcon
+                  backgroundColor="darkBlue"
+                  color="white"
+                  title="La asociación"
+                  Icon={FaHandshakeSimple}
+                  iconColor="white"
+                />
+              </Link>
+            </div>
+          </div>
+        </section>
         <Footer></Footer>
+      </div>
     </>
   );
 };

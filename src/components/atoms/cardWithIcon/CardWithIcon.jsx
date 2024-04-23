0db87bbfb4bd084c8ae516e4498fce05b6ddb88a
KalypsoDev <antunezangela89@gmail.com>
2024-04-23 22:36:05 +0200

    Fix styles (media queries and scroll)

--- src/components/atoms/cardWithIcon/CardWithIcon.jsx
+++ src/components/atoms/cardWithIcon/CardWithIcon.jsx
@@ -13,10 +13,10 @@ const CardWithIcon = ({
 
   return (
 
-      <div className={`flex flex-col justify-center m-4 w-56 h-48 md:w-72 md:h-72 p-6 bg-${backgroundColor} border border-gray-200 rounded-lg`} style={shadowStyle}>
-            <h3 className={`text-lg p-4 md:text-xl font-montserratBold text-center mb-4 text-${color}`}>{title}</h3>
-            {Icon && <Icon className={`text-${iconColor} text-8xl mx-auto`} />}
-            {subtitle !== undefined && <p className={`text-lg p-4 md:text-xl font-montserratRegular text-center mb-4 text-${color}`}>{subtitle}</p>}
+      <div className={`flex flex-col justify-center p-4 w-64 h-48 md:w-96 md:h-72 bg-${backgroundColor} border border-gray-200 rounded-lg`} style={shadowStyle}>
+            <h3 className={`text-base p-2 md:text-xl font-montserratBold text-center mb-4 text-${color}`}>{title}</h3>
+            {Icon && <Icon className={`text-${iconColor} text-6xl md:text-8xl mx-auto`} />}
+            {subtitle !== undefined && <p className={`text-xs p-3 md:p-4 md:text-lg font-montserratRegular text-center text-${color}`}>{subtitle}</p>}
       </div>
   );
 };

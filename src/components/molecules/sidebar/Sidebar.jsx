63cb29a53630f5afdfcdca3014e0d14ecb48f3ab
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-04-24 11:57:03 +0200

    Modify Sidebar component styles

--- src/components/molecules/sidebar/Sidebar.jsx
+++ src/components/molecules/sidebar/Sidebar.jsx
@@ -72,7 +72,7 @@ const Sidebar = () => {
                     </div>
 
                     <div className="flex justify-center">
-                        <button type="button" className="text-white font-montserratBold text-sm bg-darkBlue hover:bg-primaryBlue focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 mt-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <button type="button" className="text-white font-montserratBold text-sm bg-darkBlue focus:ring-4 focus:ring-blue-300 rounded-lg px-5 py-2.5 me-2 mb-2 mt-2 focus:outline-none">
                             Cerrar sesión
                         </button>
                     </div>

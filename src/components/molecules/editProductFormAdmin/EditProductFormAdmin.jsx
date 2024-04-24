851fe70d4e00381fafbd3d3cbebd8201faa40bc8
Desiree <desireesanchezdev@gmail.com>
2024-04-24 08:27:05 +0200

    Change styles on EditProductFormAdmin

--- src/components/molecules/editProductFormAdmin/EditProductFormAdmin.jsx
+++ src/components/molecules/editProductFormAdmin/EditProductFormAdmin.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import FetchApi from '../../../services/FetchApi';
 import { useParams } from 'react-router-dom';
 import Button from '../../atoms/button/Button';
+import { Link } from "react-router-dom";
 
 const EditProductFormAdmin = (tu ) => {
     const { id } = useParams();
@@ -83,7 +84,7 @@ const EditProductFormAdmin = (tu ) => {
             <div className="flex flex-col sm:flex-row w-full">
                 <div className="flex flex-col w-full sm:w-2/3 justify-center">
                     <div className="mb-3 w-full sm:w-4/5">
-                        <label htmlFor="name" className="block mb-2 text-lg text-white text-center font-montserratRegular">Nombre</label>
+                        <label htmlFor="name" className="block mb-2 text-base text-white text-center font-montserratRegular">Nombre</label>
                         <input
                             type="text"
                             id="name"
@@ -95,7 +96,7 @@ const EditProductFormAdmin = (tu ) => {
                         />
                     </div>
                     <div className="mb-3 w-full sm:w-4/5">
-                        <label htmlFor="stock" className="block mb-2 text-lg text-center text-white font-montserratRegular">Cantidad</label>
+                        <label htmlFor="stock" className="block mb-2 text-base text-center text-white font-montserratRegular">Cantidad</label>
                         <input
                             type="number"
                             id="stock"
@@ -107,7 +108,7 @@ const EditProductFormAdmin = (tu ) => {
                         />
                     </div>
                     <div className="mb-3 w-full sm:w-4/5">
-                        <label htmlFor="price" className="block mb-2 text-lg text-center text-white font-montserratRegular">Precio</label>
+                        <label htmlFor="price" className="block mb-2 text-base text-center text-white font-montserratRegular">Precio</label>
                         <input
                             type="number"
                             id="price"
@@ -119,25 +120,25 @@ const EditProductFormAdmin = (tu ) => {
                         />
                     </div>
                     <div className="mb-3 w-full sm:w-4/5">
-                        <label htmlFor="description" className="block mb-2 text-lg text-center text-white font-montserratRegular">Descripción</label>
+                        <label htmlFor="description" className="block mb-2 text-base text-center text-white font-montserratRegular">Descripción</label>
                         <textarea
                             id="description"
                             name="description"
                             value={product.description}
                             onChange={(e) => handleFieldChange('description', e.target.value)}
-                            className="bg-white border border-opacity-20 border-gray-300 text-darkGrey text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 sm:text-lg resize-none"
+                            className="bg-white border border-opacity-20 border-gray-300 text-darkGrey text-sm text-center rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 sm:text-lg resize-none"
                             required
                         />
                     </div>
                 </div>
                 <div className="flex flex-col w-full sm:w-1/2 items-center mb-3 sm:mb-0">
-                    <h2 className="text-center text-lg text-white mb-2 font-montserratRegular">Imagen</h2>
+                    <h2 className="text-center text-base text-white mb-2 font-montserratRegular">Imagen</h2>
                     <img
                         className="w-full h-60 mb-5 rounded-lg"
                         src={product.image_url}
                         alt="Imagen de Producto"
                     />
-                    <label htmlFor="image_url" className="block text-sm font-medium text-white text-center"></label>
+                    <label htmlFor="image_url" className="block text-base text-white text-center"></label>
                     <input
                         type="file"
                         id="image_url"
@@ -149,20 +150,17 @@ const EditProductFormAdmin = (tu ) => {
                 </div>
             </div>
             <div>
-
                 <Button
                     type="submit"
-                    className="text-white font-montserratBold bg-darkBlue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-6 py-2 mt-5 mr-5"
+                    className="text-white font-montserratBold bg-darkBlue text-base focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-6 py-2 mt-5 mr-5"
                     text="Editar" />
                 <Link to="/admin/productos">
                     <Button
                         type="submit"
-                        className="text-white font-montserratBold bg-redBin focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-6 py-2 mt-5 mr-5"
+                        className="text-white font-montserratBold bg-redBin text-base focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-6 py-2 mt-5 mr-5"
                         text="Cancelar"
                         />
                 </Link>
-                {/* <button type="submit" className="text-white font-montserratBold bg-darkBlue hover:bg-primaryBlue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-6 py-2 mt-5 mr-5">Guardar Cambios</button> */}
-                {/* <button type="button" onClick={onCancel} className="text-primaryLila font-montserratBold bg-white hover:text-white hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-6 py-2 mt-5">Cancelar</button> */}
             </div>
         </form>
     );
